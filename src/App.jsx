525e17539775f8f042a8571cce9c7856e8c1765f
Guilherme <111528193+guihi1@users.noreply.github.com>
2023-08-17 13:25:29 -0300

    Fix useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,35 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [data, setData] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon/110/').
-        then(function(response) {
-          return response.json();
-        }).
-        then(function(response) {
-          console.log(response);
-        })
-      return response;
-    }
-    fetchData();
-  }, [])
+    let ignore = false;
 
+    const fetchData = async (i) => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+      const data = await response.json();
+      if (!ignore) {
+        setPokemonList((prevList) => ([...prevList, data]));
+      }
+    }
+
+    for (let i = 110; i < 118; i += 1) {
+      fetchData(i);
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  console.log(pokemonList);
   return (
-    <div></div>
+    <div>
+      {pokemonList.map((pokemon) => (
+        <Card key={pokemon.name} name={pokemon.name} image={pokemon.sprites.other["official-artwork"].front_default} />
+      ))}
+    </div>
   )
 }
 
